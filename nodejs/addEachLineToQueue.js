3001a2835f91ed1170eff6742fcf011dae0a9f4b
Aoyama, Shotaro <aoyama@val.co.jp>
2019-02-17 02:36:35 +0900

    new Promise()をasyncで置き換え

--- nodejs/addEachLineToQueue.js
+++ nodejs/addEachLineToQueue.js
@@ -13,45 +13,42 @@ const sqs = new AWS.SQS({apiVersion: '2012-11-05', region: 'ap-northeast-1'});
  * Failedが返ってきた場合は1回だけリトライする。
  * リトライも失敗した場合は例外を投げる。
  */
-function sendMessageBatch(queueUrl, messageBodies) {
-  return new Promise(async (resolve, reject) => {
-    if (messageBodies.length > 10) {
-      reject(`messageBodies.length > 10 : ${messageBodies}`);
-    }
-    if (messageBodies.length == 0) {
-      return resolve(messageBodies);
-    }
+async function sendMessageBatch(queueUrl, messageBodies) {
+  if (messageBodies.length > 10) {
+    throw `messageBodies.length > 10 : ${messageBodies}`;
+  }
+  if (messageBodies.length == 0) {
+    return messageBodies;
+  }
 
-    const TRY_MAX = 2;
-    let tryCount = 0;
-    const allEntries = messageBodies.map(function(messageBody, index) {
-      return {
-        Id: String(index),
-        MessageBody: messageBody,
-      };
-    });
-    let entries = allEntries;
-
-    while (true) {
-      tryCount += 1;
-      var params = {
-        Entries: entries,
-        QueueUrl: queueUrl,
-      };
-      let result = await sqs.sendMessageBatch(params).promise();
-      if (result.Failed.length == 0) {
-        resolve(messageBodies);
-        return;
-      }
-      if (tryCount >= TRY_MAX) {
-        throw new Error('tryCount >= TRY_MAX ' + result.Failed);
-      }
-      entries = result.Failed.map(function(failed) {
-        var id = Number(failed.Id);
-        return allEntries[id];
-      });
-    }
+  const TRY_MAX = 2;
+  let tryCount = 0;
+  const allEntries = messageBodies.map(function(messageBody, index) {
+    return {
+      Id: String(index),
+      MessageBody: messageBody,
+    };
   });
+  let entries = allEntries;
+
+  while (true) {
+    tryCount += 1;
+    var params = {
+      Entries: entries,
+      QueueUrl: queueUrl,
+    };
+    let result = await sqs.sendMessageBatch(params).promise();
+    if (result.Failed.length == 0) {
+      return messageBodies;
+    }
+    if (tryCount >= TRY_MAX) {
+      throw new Error('tryCount >= TRY_MAX ' + result.Failed);
+    }
+    entries = result.Failed.map(function(failed) {
+      var id = Number(failed.Id);
+      return allEntries[id];
+    });
+  }
 }
 
 /**
@@ -60,26 +57,23 @@ function sendMessageBatch(queueUrl, messageBodies) {
  * @param {String} filename 例: 'list.txt'
  * @param {String} queueUrl 例: 'https://sqs.ap-northeast-1.amazonaws.com/XXXXXXXXXXXXXXX/MY-QUEUE-NAME'
  */
-function addEachLineToQueue(filename, queueUrl) {
-  return new Promise(async (resolve, reject) => {
-    let lines = fs.readFileSync(filename).toString().split('\n').map(line => line.replace(/\s+$/g, '')).filter(line => line != '');
-    const PROMISES_SIZE = 100;
-    let i = 0;
-    while (i < lines.length) {
-      let promises = [];
-      for (let j = 0; j < PROMISES_SIZE && i < lines.length; j++) {
-        let messageBodies = lines.slice(i, i + 10);
-        promises.push(sendMessageBatch(queueUrl, messageBodies));
-        i += 10;
-      }
-      let results = await Promise.all(promises);
-      var time = new Date();
-      results.forEach(function(messageBodies) {
-        console.log(time.toISOString(), '   ', messageBodies.join(','));
-      });
+async function addEachLineToQueue(filename, queueUrl) {
+  let lines = fs.readFileSync(filename).toString().split('\n').map(line => line.replace(/\s+$/g, '')).filter(line => line != '');
+  const PROMISES_SIZE = 100;
+  let i = 0;
+  while (i < lines.length) {
+    let promises = [];
+    for (let j = 0; j < PROMISES_SIZE && i < lines.length; j++) {
+      let messageBodies = lines.slice(i, i + 10);
+      promises.push(sendMessageBatch(queueUrl, messageBodies));
+      i += 10;
     }
-    resolve();
-  });
+    let results = await Promise.all(promises);
+    var time = new Date();
+    results.forEach(function(messageBodies) {
+      console.log(time.toISOString(), '   ', messageBodies.join(','));
+    });
+  }
 }
 
 async function main() {
@@ -94,7 +88,11 @@ async function main() {
 
   var started = new Date();
 
-  await addEachLineToQueue(FILENAME, QUEUE_URL);
+  try {
+    await addEachLineToQueue(FILENAME, QUEUE_URL);
+  } catch (ex) {
+    console.log('予期しないエラー', ex);
+  }
 
   var finished = new Date();
   console.log("Elapsed:" + (finished - started) + " ms");
